35f0c430a5bc7926c9024620ed7d2c37781dff07
Frederic Chucholowski <39057944+fchucholowski@users.noreply.github.com>
2020-01-11 10:07:16 +0100

    Use VisualStudio 2017 to build sfun_fmurun

--- build.bat
+++ build.bat
@@ -4,13 +4,13 @@ call mvn -f Java/pom.xml install
 echo build the generic S-function (sfun_fmurun.mexw32)
 mkdir win32
 cd win32
-cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 cd ..
 
 echo build the generic S-function (sfun_fmurun.mexw64)
 mkdir win64
 cd win64
-cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 cd ..
